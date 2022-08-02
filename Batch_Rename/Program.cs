2353a890f0df1c0905fa297df45b9e5e1872e6ca
nekun1 <nekun1@wp.pl>
2022-08-02 15:11:08 +0200

    Initial commit

---
+++ Batch_Rename/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Batch_Rename
+{
+    class Program
+	{
+        static void Main()
+	    {
+            List<string> filenames = new();
+            string path = ReceiveInput("Please enter the file's full path: ");
+            AddFilenames(path);
+
+        } 
+        static string ReceiveInput(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+        static void AddFilenames(string path)
+        {
+            List<string> filenames = new();
+            foreach (string line in File.ReadLines(@path))
+            {
+                filenames.Add(line);
+            }
+        }
+        static void Rename()
+        {
+            int index = 1;
+            foreach (string filename in filenames)
+            {
+                File.Move(@$"G:\Gry\Darkest Dungeon\audio\secondary_banks\Fmod Bank Tools\wav\{filename}.wav", @$"G:\Gry\Darkest Dungeon\audio\secondary_banks\Fmod Bank Tools\wav\{filename}");
+                index++;
+            }
+        }
+	}
+}
